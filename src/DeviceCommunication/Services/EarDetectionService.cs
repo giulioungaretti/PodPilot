b35b89b1a9dcb33e6844a7d9632d67824c01bec6
Giulio Ungaretti <giulio.ungaretti@gmail.com>
2025-12-07 21:58:45 +0100

    fix(services): remove incorrect disposal of DI-owned GlobalMediaController

--- src/DeviceCommunication/Services/EarDetectionService.cs
+++ src/DeviceCommunication/Services/EarDetectionService.cs
@@ -185,6 +185,6 @@ public sealed class EarDetectionService : IDisposable
         _disposed = true;
 
         _discoveryService.DeviceUpdated -= OnDeviceUpdated;
-        _mediaController.Dispose();
+        // Note: Do NOT dispose _mediaController - it's owned by the DI container
     }
 }
