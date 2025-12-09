574541149e278826ca412aee227b6320928404f2
Giulio Ungaretti <giulio.ungaretti@gmail.com>
2025-12-09 13:43:56 +0100

    refactor(test): update DeviceCommunication.Tests to use PodPilot.Core

--- src/DeviceCommunication.Tests/Services/AirPodsStateServiceTests.cs
+++ src/DeviceCommunication.Tests/Services/AirPodsStateServiceTests.cs
@@ -4,6 +4,8 @@ using DeviceCommunication.Services;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using PodPilot.Core.Models;
+using TestHelpers;
 using Xunit;
 
 namespace DeviceCommunication.Tests.Services;
@@ -20,12 +22,6 @@ public class AirPodsStateServiceTests : IAsyncLifetime
     private readonly IBleDataProvider _bleDataProvider;
     private readonly IDefaultAudioOutputMonitorService _audioOutputMonitor;
     private readonly AirPodsStateService _sut;
-
-    // Test data
-    private const ushort AirPodsPro2ProductId = 0x2014;
-    private const string TestDeviceName = "Test AirPods Pro";
-    private const ulong TestBluetoothAddress = 0xAABBCCDDEEFF;
-    private const ulong TestBleAddress = 0x112233445566;
 
     public AirPodsStateServiceTests()
     {
@@ -67,21 +63,21 @@ public class AirPodsStateServiceTests : IAsyncLifetime
     public void GetAllDevices_WhenDeviceIsPairedAndSeenViaBle_ReturnsSingleDevice()
     {
         // Arrange: Simulate paired device being added
-        var pairedDevice = CreatePairedDevice();
-        RaisePairedDeviceAdded(pairedDevice);
+        var pairedDevice = AirPodsStateServiceTestHelpers.CreatePairedDevice();
+        AirPodsStateServiceTestHelpers.RaisePairedDeviceAdded(_pairedDeviceWatcher, pairedDevice);
 
         // Act: Simulate BLE data arriving for the same ProductId
-        var bleData = CreateBleData();
-        RaiseBleDataReceived(bleData);
+        var bleData = AirPodsStateServiceTestHelpers.CreateBleData();
+        AirPodsStateServiceTestHelpers.RaiseBleDataReceived(_bleDataProvider, bleData);
 
         // Assert: Should have exactly ONE device
         var allDevices = _sut.GetAllDevices();
         allDevices.Should().HaveCount(1, "paired device + BLE data for same ProductId should be merged");
         
         var device = allDevices[0];
-        device.ProductId.Should().Be(AirPodsPro2ProductId);
+        device.ProductId.Should().Be(AirPodsStateServiceTestHelpers.AirPodsPro2ProductId);
         device.IsPaired.Should().BeTrue("device is from paired API");
-        device.Name.Should().Be(TestDeviceName, "should use paired device name");
+        device.Name.Should().Be(AirPodsStateServiceTestHelpers.TestDeviceName, "should use paired device name");
         device.LeftBattery.Should().Be(80, "should have BLE enrichment data");
     }
 
@@ -93,16 +89,16 @@ public class AirPodsStateServiceTests : IAsyncLifetime
     public void GetAllDevices_WhenBleDataArrivesBeforePairing_MergesIntoSingleDevice()
     {
         // Arrange: BLE data arrives first (unpaired)
-        var bleData = CreateBleData();
-        RaiseBleDataReceived(bleData);
+        var bleData = AirPodsStateServiceTestHelpers.CreateBleData();
+        AirPodsStateServiceTestHelpers.RaiseBleDataReceived(_bleDataProvider, bleData);
 
         // Initially should have one unpaired device
         _sut.GetAllDevices().Should().HaveCount(1);
         _sut.GetAllDevices()[0].IsPaired.Should().BeFalse();
 
         // Act: Then paired device is enumerated
-        var pairedDevice = CreatePairedDevice();
-        RaisePairedDeviceAdded(pairedDevice);
+        var pairedDevice = AirPodsStateServiceTestHelpers.CreatePairedDevice();
+        AirPodsStateServiceTestHelpers.RaisePairedDeviceAdded(_pairedDeviceWatcher, pairedDevice);
 
         // Assert: Should STILL have exactly one device, now paired
         var allDevices = _sut.GetAllDevices();
@@ -110,7 +106,7 @@ public class AirPodsStateServiceTests : IAsyncLifetime
         
         var device = allDevices[0];
         device.IsPaired.Should().BeTrue("device should now be marked as paired");
-        device.Name.Should().Be(TestDeviceName, "should use paired device name");
+        device.Name.Should().Be(AirPodsStateServiceTestHelpers.TestDeviceName, "should use paired device name");
     }
 
     /// <summary>
@@ -121,12 +117,12 @@ public class AirPodsStateServiceTests : IAsyncLifetime
     public void GetPairedDevices_OnlyReturnsPairedDevices()
     {
         // Arrange: Add one paired device
-        var pairedDevice = CreatePairedDevice();
-        RaisePairedDeviceAdded(pairedDevice);
+        var pairedDevice = AirPodsStateServiceTestHelpers.CreatePairedDevice();
+        AirPodsStateServiceTestHelpers.RaisePairedDeviceAdded(_pairedDeviceWatcher, pairedDevice);
 
         // Add one unpaired device (different product ID)
-        var unpairedBleData = CreateBleData(productId: 0x200F); // AirPods 2
-        RaiseBleDataReceived(unpairedBleData);
+        var unpairedBleData = AirPodsStateServiceTestHelpers.CreateBleData(productId: AirPodsStateServiceTestHelpers.AirPods2ProductId);
+        AirPodsStateServiceTestHelpers.RaiseBleDataReceived(_bleDataProvider, unpairedBleData);
 
         // Assert
         var allDevices = _sut.GetAllDevices();
@@ -134,7 +130,7 @@ public class AirPodsStateServiceTests : IAsyncLifetime
 
         allDevices.Should().HaveCount(2, "should have paired + unpaired");
         pairedDevices.Should().HaveCount(1, "should only have paired device");
-        pairedDevices[0].ProductId.Should().Be(AirPodsPro2ProductId);
+        pairedDevices[0].ProductId.Should().Be(AirPodsStateServiceTestHelpers.AirPodsPro2ProductId);
     }
 
     /// <summary>
@@ -150,16 +146,16 @@ public class AirPodsStateServiceTests : IAsyncLifetime
         _sut.StateChanged += (_, e) => receivedStates.Add(e);
 
         // Act: Paired device added
-        var pairedDevice = CreatePairedDevice();
-        RaisePairedDeviceAdded(pairedDevice);
+        var pairedDevice = AirPodsStateServiceTestHelpers.CreatePairedDevice();
+        AirPodsStateServiceTestHelpers.RaisePairedDeviceAdded(_pairedDeviceWatcher, pairedDevice);
 
         // Act: BLE data for same device
-        var bleData = CreateBleData();
-        RaiseBleDataReceived(bleData);
+        var bleData = AirPodsStateServiceTestHelpers.CreateBleData();
+        AirPodsStateServiceTestHelpers.RaiseBleDataReceived(_bleDataProvider, bleData);
 
         // Assert: Events should all be for the SAME ProductId
         receivedStates.Should().HaveCountGreaterThan(0);
-        receivedStates.All(e => e.State.ProductId == AirPodsPro2ProductId)
+        receivedStates.All(e => e.State.ProductId == AirPodsStateServiceTestHelpers.AirPodsPro2ProductId)
             .Should().BeTrue("all events should be for the same device");
     }
 
@@ -181,12 +177,12 @@ public class AirPodsStateServiceTests : IAsyncLifetime
     public void DocumentCurrentBehavior_PairedDeviceAppearsInBothCollections()
     {
         // Arrange: Simulate paired device
-        var pairedDevice = CreatePairedDevice();
-        RaisePairedDeviceAdded(pairedDevice);
+        var pairedDevice = AirPodsStateServiceTestHelpers.CreatePairedDevice();
+        AirPodsStateServiceTestHelpers.RaisePairedDeviceAdded(_pairedDeviceWatcher, pairedDevice);
         
         // Simulate BLE data
-        var bleData = CreateBleData();
-        RaiseBleDataReceived(bleData);
+        var bleData = AirPodsStateServiceTestHelpers.CreateBleData();
+        AirPodsStateServiceTestHelpers.RaiseBleDataReceived(_bleDataProvider, bleData);
 
         // Act: Simulate what MainPageViewModel does
         var pairedDevices = _sut.GetPairedDevices();
@@ -201,8 +197,8 @@ public class AirPodsStateServiceTests : IAsyncLifetime
         allDevices.Should().HaveCount(1);
 
         // The same device appears in both collections
-        var inPaired = pairedDevices.Any(d => d.ProductId == AirPodsPro2ProductId);
-        var inAll = allDevices.Any(d => d.ProductId == AirPodsPro2ProductId);
+        var inPaired = pairedDevices.Any(d => d.ProductId == AirPodsStateServiceTestHelpers.AirPodsPro2ProductId);
+        var inAll = allDevices.Any(d => d.ProductId == AirPodsStateServiceTestHelpers.AirPodsPro2ProductId);
 
         inPaired.Should().BeTrue("device is in paired collection");
         inAll.Should().BeTrue("device is also in all devices collection");
@@ -225,11 +221,11 @@ public class AirPodsStateServiceTests : IAsyncLifetime
     public void ExpectedBehavior_NearbyDevicesShouldExcludePaired()
     {
         // Arrange: One paired, one unpaired
-        var pairedDevice = CreatePairedDevice();
-        RaisePairedDeviceAdded(pairedDevice);
+        var pairedDevice = AirPodsStateServiceTestHelpers.CreatePairedDevice();
+        AirPodsStateServiceTestHelpers.RaisePairedDeviceAdded(_pairedDeviceWatcher, pairedDevice);
         
-        var unpairedBleData = CreateBleData(productId: 0x200F); // AirPods 2 - unpaired
-        RaiseBleDataReceived(unpairedBleData);
+        var unpairedBleData = AirPodsStateServiceTestHelpers.CreateBleData(productId: AirPodsStateServiceTestHelpers.AirPods2ProductId);
+        AirPodsStateServiceTestHelpers.RaiseBleDataReceived(_bleDataProvider, unpairedBleData);
 
         // Act: Calculate what "Nearby Devices" SHOULD show
         var allDevices = _sut.GetAllDevices();
@@ -237,64 +233,8 @@ public class AirPodsStateServiceTests : IAsyncLifetime
 
         // Assert: Only unpaired device should be in "Nearby"
         nearbyOnly.Should().HaveCount(1);
-        nearbyOnly[0].ProductId.Should().Be(0x200F);
+        nearbyOnly[0].ProductId.Should().Be(AirPodsStateServiceTestHelpers.AirPods2ProductId);
         nearbyOnly[0].IsPaired.Should().BeFalse();
-    }
-
-    #endregion
-
-    #region Helpers
-
-    private PairedDeviceInfo CreatePairedDevice(ushort productId = AirPodsPro2ProductId)
-    {
-        return new PairedDeviceInfo
-        {
-            Id = "device-id-123",
-            ProductId = productId,
-            Name = TestDeviceName,
-            Address = TestBluetoothAddress,
-            IsConnected = false
-        };
-    }
-
-    private BleEnrichmentData CreateBleData(ushort productId = AirPodsPro2ProductId)
-    {
-        return new BleEnrichmentData
-        {
-            ProductId = productId,
-            BleAddress = TestBleAddress,
-            ModelName = AppleDeviceModelHelper.GetDisplayName(AppleDeviceModel.AirPodsPro2),
-            LeftBattery = 80,
-            RightBattery = 90,
-            CaseBattery = 100,
-            IsLeftCharging = false,
-            IsRightCharging = false,
-            IsCaseCharging = false,
-            IsLeftInEar = false,
-            IsRightInEar = false,
-            IsLidOpen = true,
-            IsBothPodsInCase = true,
-            SignalStrength = -50,
-            LastUpdate = DateTime.Now
-        };
-    }
-
-    private void RaisePairedDeviceAdded(PairedDeviceInfo device)
-    {
-        _pairedDeviceWatcher.DeviceChanged += Raise.Event<EventHandler<PairedDeviceChangedEventArgs>>(
-            _pairedDeviceWatcher,
-            new PairedDeviceChangedEventArgs
-            {
-                Device = device,
-                ChangeType = PairedDeviceChangeType.Added
-            });
-    }
-
-    private void RaiseBleDataReceived(BleEnrichmentData data)
-    {
-        _bleDataProvider.DataReceived += Raise.Event<EventHandler<BleEnrichmentData>>(
-            _bleDataProvider,
-            data);
     }
 
     #endregion
